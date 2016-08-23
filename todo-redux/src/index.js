9a46a46c937867dfa5caa0912f5d5ab2b59914c6
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-24 00:14:47 +0700

    Render App with redux Provider

---
+++ todo-redux/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import App from './components/App'
+
+let store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+)
