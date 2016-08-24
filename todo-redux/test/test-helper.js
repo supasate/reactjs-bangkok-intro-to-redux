4c2911459a3b682688b446dfc97e3d0974fb7b3b
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-25 00:06:57 +0700

    Prepare test environment for enzyme's mount

--- todo-redux/test/test-helper.js
+++ todo-redux/test/test-helper.js
@@ -3,9 +3,14 @@ import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import { mount, render, shallow } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
+import jsdom from 'jsdom'
 
 chai.use(sinonChai)
 chai.use(chaiEnzyme())
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc
+global.window = doc.defaultView
 
 export {
   expect,
