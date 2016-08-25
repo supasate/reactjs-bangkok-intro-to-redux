3860188d2e167afcbe058e0311c6960a4f64c982
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-08-26 02:44:39 +0700

    Fix hot reload

--- todo-redux/webpack.config.js
+++ todo-redux/webpack.config.js
@@ -6,20 +6,19 @@ module.exports = {
   devtool: 'eval-source-map',
   entry: {
     entry: path.join(__dirname, 'src', 'index.js'),
-    webpackdev: 'webpack-dev-server/client?http://localhost:8080',
     vendor: ['react', 'react-dom'],
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: 'dist',
-    filename: '[name].[chunkhash].js',
+    publicPath: '/dist/',
+    filename: '[name].[hash].js',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel'],
+        loaders: ['react-hot', 'babel'],
       },
       {
         test: /\.css?$/,
