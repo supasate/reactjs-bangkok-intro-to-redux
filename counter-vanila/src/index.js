c10d0f2d36cf85207885114ec232d2cdd7b34849
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-08-27 12:02:37 +0700

    Add counter-vanila

--- counter-vanila/src/index.js
+++ counter-vanila/src/index.js
@@ -2,27 +2,64 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 
-import ParentComponent from './ParentComponent'
+const add = (value) => {
+  return { type: 'ADD', payload: value }
+}
 
-
-const initialState = { num: 0 }
-const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'ADD': { 
-      return {
-        ...state,
-        num: state.num + action.payload
-      }
-    }
-    default: {
-      return state
-    } 
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
   }
 }
 
-const store = createStore(reducer)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    count: () => dispatch(add(1))
+  }
+}
 
-ReactDOM.render(
-  <ParentComponent store={store} />,
-  document.getElementById('root')
+const reducer = (state = { counter: 0}, action) => {
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + action.payload,
+    }
+  }
+  return state
+}
+
+const store = createStore(reducer)
+const connect = (mapStateToProps, mapDispatchToProps) => {
+  return (Presentational) => {
+    return class extends Component {
+      constructor(props) {
+        super(props)
+        const stateProps = mapStateToProps(store.getState())
+        const dispatchProps = mapDispatchToProps(store.dispatch)
+        this.state = {
+          ...stateProps,
+          ...dispatchProps,
+        }
+      }
+      componentWillMount() {
+        store.subscribe(() => {
+          const stateProps = mapStateToProps(store.getState())
+          console.log(stateProps)
+          this.setState(stateProps)
+        })
+      }
+      render() {
+        return <Presentational {...this.state} />
+      }
+    }
+  }
+}
+
+const Counter = (props) => (
+  <div>
+    <label>{props.counter}</label>
+    <button onClick={props.count}>Add</button>
+  </div>
 )
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter)
+ReactDOM.render(<CounterContainer />, document.getElementById('root'))
